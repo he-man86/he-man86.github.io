296649ff1ca55d9e80813966b4205688ed95da5b
he-man86 <108829286+he-man86@users.noreply.github.com>
2022-07-17 05:55:49 +0200

    Update JoyStick.js

--- libs/JoyStick.js
+++ libs/JoyStick.js
@@ -49,8 +49,12 @@ class JoyStick{
 		this.offset = this.getMousePosition(evt);
 		const joystick = this;
 		if ('ontouchstart' in window){
-			document.ontouchmove = function(evt){ evt.preventDefault(); joystick.move(evt); };
-			document.ontouchend =  function(evt){ evt.preventDefault(); joystick.up(evt); };
+			document.ontouchmove = function(evt){ 
+				//evt.preventDefault(); 
+				joystick.move(evt); };
+			document.ontouchend =  function(evt){ 
+				//evt.preventDefault(); 
+				joystick.up(evt); };
 		}else{
 			document.onmousemove = function(evt){ evt.preventDefault(); joystick.move(evt); };
 			document.onmouseup = function(evt){ evt.preventDefault(); joystick.up(evt); };
